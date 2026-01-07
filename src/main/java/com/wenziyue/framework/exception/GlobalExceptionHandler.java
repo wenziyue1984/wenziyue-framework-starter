1dc3d75a0c0801decf99252925c162f618fcb9dc
wenziyue <wenziyue1984@163.com>
2026-01-07 17:43:23 +0800

    release: v1.0.8

--- src/main/java/com/wenziyue/framework/exception/GlobalExceptionHandler.java
+++ src/main/java/com/wenziyue/framework/exception/GlobalExceptionHandler.java
@@ -1,9 +1,13 @@
 package com.wenziyue.framework.exception;
 
 import com.wenziyue.framework.common.ApiResult;
+import com.wenziyue.framework.common.FeignConstants;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author wenziyue
@@ -13,15 +17,34 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public ApiResult<String> handleException(Exception e) {
+    public Object handleException(Exception e, HttpServletRequest request) {
         log.error("系统异常：{}", e.getMessage(), e);
+        if (isFeignCall(request)) {
+            return ResponseEntity.status(500).body("Internal server error");
+        }
         return ApiResult.error("Server error: " + e.getMessage());
     }
 
     // 处理自定义的业务异常
     @ExceptionHandler(ApiException.class)
-    public ApiResult<String> handleBusinessException(ApiException e) {
+    public Object handleBusinessException(ApiException e, HttpServletRequest request) {
         log.warn("业务异常：{} - {}", e.getCode(), e.getMessage(), e);
+        if (isFeignCall(request)) {
+            int status = toHttpStatus(e.getCode());
+            return ResponseEntity.status(status).body(e.getMessage());
+        }
         return ApiResult.error(e.getCode(), e.getMessage());
+    }
+
+    private boolean isFeignCall(HttpServletRequest request) {
+        return request.getHeader(FeignConstants.FEIGN_HEADER) != null;
+    }
+
+    private int toHttpStatus(String code) {
+        try {
+            return Integer.parseInt(code);
+        } catch (Exception ignore) {
+            return 500;
+        }
     }
 }
